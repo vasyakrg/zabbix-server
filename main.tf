0ef4cade94a63da9b3bb9f059a517f6cd255ebdb
Vassiliy Yegorov <vasyakrg@gmail.com>
2019-07-13 11:11:55 +0700

    - firewall move to vpc.tf

--- main.tf
+++ main.tf
@@ -1,24 +1,4 @@
 #===============================================
-# Create firefall for all
-#===============================================
-resource "google_compute_firewall" "firewall_vpn" {
-  name = "allow-zabbix"
-
-  # name of net
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports = [
-      "443", "80", "10050"
-    ]
-  }
-
-  source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["${var.zabbix_tag}"]
-}
-
-#===============================================
 # Create zabbix server
 #===============================================
 resource "google_compute_instance" "zabbix" {
